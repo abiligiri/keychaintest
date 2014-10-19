cd22e3e881a16fbc55aa248143fb7fc164a88d40
Anand Biligiri <joywhitehill-github@yahoo.com>
2014-10-19 09:43:19 -0700

    add keychain wrapper and gitignore

--- keychaintest/ViewController.m
+++ keychaintest/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "KeychainItemWrapper.h"
 @interface ViewController ()
 
 @end
@@ -19,6 +19,16 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    KeychainItemWrapper *wrapper = [[KeychainItemWrapper alloc] initWithIdentifier:@"testidentifier" accessGroup:nil];
+    [wrapper setObject:@"testservice" forKey:(__bridge NSString*)kSecAttrService];
+    [wrapper setObject:@"testaccount" forKey:(__bridge NSString*)kSecAttrAccount];
+    [wrapper setObject:(__bridge NSString*)kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly forKey:(__bridge NSString*)kSecAttrAccessible];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
